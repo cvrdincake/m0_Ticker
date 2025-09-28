b10ff8af54b241422a1b39a7c9bf85fb2e1e7dca
m0²ƶe. <112899414+cvrdincake@users.noreply.github.com>
2025-09-28 02:10:43 +0000

    refactor: update eslint.config.cjs to use flat config and remove legacy references

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,32 +1,7 @@
 // eslint.config.cjs
-const path = require("path");
-const globals = require("globals");
-const htmlPlugin = require("eslint-plugin-html");
 const js = require("@eslint/js");
+const htmlPlugin = require("eslint-plugin-html");
 const prettierConfig = require("eslint-config-prettier");
-
-// Load legacy config for parserOptions, env, overrides, etc.
-const legacyConfig = require(path.join(__dirname, ".eslintrc.json"));
-
-const parserOptions = legacyConfig.parserOptions || {};
-const baseEnv = legacyConfig.env || {};
-
-function resolveGlobals(env = {}) {
-  return {
-    ...(env.browser ? globals.browser : {}),
-    ...(env.node ? globals.node : {}),
-  };
-}
-
-const baseRules = {
-  ...js.configs.recommended.rules,
-  ...(prettierConfig && prettierConfig.rules ? prettierConfig.rules : {}),
-  ...(legacyConfig.rules || {}),
-};
-
-const overrides = legacyConfig.overrides || [];
-const serverConfig = overrides[0] || {};
-const publicConfig = overrides[1] || {};
 
 module.exports = [
   {
@@ -35,32 +10,15 @@ module.exports = [
   {
     files: ["**/*.js"],
     languageOptions: {
-      ecmaVersion: parserOptions.ecmaVersion || "latest",
-      sourceType: parserOptions.sourceType || "script",
-      globals: resolveGlobals(baseEnv),
+      ecmaVersion: "latest",
+      sourceType: "script",
     },
     plugins: {
       html: htmlPlugin,
     },
-    rules: baseRules,
-  },
-  {
-    files: serverConfig.files || [],
-    languageOptions: {
-      ecmaVersion: parserOptions.ecmaVersion || "latest",
-      sourceType: parserOptions.sourceType || "script",
-      globals: resolveGlobals(serverConfig.env),
-    },
-  },
-  {
-    files: publicConfig.files || [],
-    languageOptions: {
-      ecmaVersion: parserOptions.ecmaVersion || "latest",
-      sourceType: parserOptions.sourceType || "script",
-      globals: {
-        ...resolveGlobals(publicConfig.env),
-        ...(publicConfig.globals || {}),
-      },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...(prettierConfig && prettierConfig.rules ? prettierConfig.rules : {}),
     },
   },
 ];
